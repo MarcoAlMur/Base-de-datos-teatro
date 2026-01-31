17cd79fbb363ab89200d484c243799d0a7643549
danii485 <danifishcloud@gmail.com>
2026-01-31 10:55:25 +0100

    Add files via upload
    Añadida la tabla butaca sesion Daniela

---
+++ daniela_butacasesion.sql
@@ -0,0 +1,11 @@
+create database estructura;
+use estructura;
+CREATE TABLE butaca_sesion (
+  id_sesion INT NOT NULL,
+  id_butaca INT NOT NULL,
+  estado ENUM('DISPONIBLE','RESERVADA','VENDIDA','BLOQUEADA') 
+         DEFAULT 'DISPONIBLE',
+  PRIMARY KEY (id_sesion, id_butaca),
+  FOREIGN KEY (id_sesion) REFERENCES sesion(id_sesion),
+  FOREIGN KEY (id_butaca) REFERENCES butaca(id_butaca)
+);
